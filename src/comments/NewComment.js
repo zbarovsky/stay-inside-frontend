754c9ce42293a99c1bc1fbd5d1abc480773cf0cc
nguyendm77 <nguyendm77@gmail.com>
2020-07-28 16:28:08 -0700

    updated Newcomment

--- src/comments/NewComment.js
+++ src/comments/NewComment.js
@@ -4,15 +4,12 @@ import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import Toast from 'react-bootstrap/Toast'
 import ToastHeader from 'react-bootstrap/ToastHeader'
-
 export default function NewComment(props) {
   let [commentCreated, setCreatedComment] = useState(false)
   let [inputs, setInputs] = useState({
     name: '',
     content: '',
   })
-
-
   let commentSubmit = e => {
     e.preventDefault()
     window.location.reload(true)
@@ -26,36 +23,34 @@ export default function NewComment(props) {
         console.log(err)
       })
     }
-
   const handleInputChange = e => {
     e.persist()
     setInputs({...inputs, [e.target.name]: e.target.value})
   }
-
   return (
     <div>
       <Toast className="mt-2">
         <ToastHeader closeButton={false}>
           <form onSubmit={commentSubmit}>
-            <strong className="mr-auto"> 
-              <div class="form-group">
-                <label for="exampleFormControlInput1">Name</label>
-                <input type="text" name='name' class="form-control" id="exampleFormControlInput1" onChange={handleInputChange} />
-              </div>
-              <div class="form-group">
-                <label for="exampleFormControlTextarea1">Comment</label>
-                <input type="text" name='content' class="form-control" id="exampleFormControlInput1" onChange={handleInputChange} />
-              </div>
-              </strong>
             <div class="form-group">
               <input hidden type="text" name='eventId' class="form-control" id="exampleFormControlInput1" value={props.id} onChange={handleInputChange} />
             </div>
+            <strong className="mr-auto"> 
+                <div class="form-group">
+                  <label for="exampleFormControlInput1">Name</label>
+                  <input type="text" name='name' class="form-control" id="exampleFormControlInput1" onChange={handleInputChange} />
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Comment</label>
+                  <input type="text" name='content' class="form-control" id="exampleFormControlInput1" onChange={handleInputChange} />
+                </div>
+              </strong>
             {/* <div class="form-group">
-              <input hidden type="text" name='postedBy' class="form-control" id="exampleFormControlInput1" value={props.user._id} onChange={handleInputChange} />
+              <input hidden type="text" name={props.user._id} class="form-control"  onChange={handleInputChange} />
             </div> */}
+            <Button variant="info" className='btn ml-3' type='submit'>Add Comment</Button>
           </form>
         </ToastHeader>  
-            <Button variant="info" className='btn ml-3' type='submit'>Add Comment</Button>
         <Toast.Body> </Toast.Body>
       </Toast>
     </div>
