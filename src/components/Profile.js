1428c4b97c081dbd8a29bb1c1d8fae4d1cde19a0
Nick Hvattum <nhvattum@gmail.com>
2020-07-29 13:07:18 -0700

    events and comments render properly

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,7 +10,7 @@ const Profile = (props) => {
   let [events, setEvents] = useState([])
 
   useEffect(()=>{
-      axios.get('http://localhost:3000/events', events)
+      axios.post('http://localhost:3000/events', {data: {events: events, user: props.user}})
       .then(response => {
           setEvents(response.data)
           console.log(response)
@@ -34,27 +34,27 @@ const Profile = (props) => {
           <h3>Create a <Link to='/events/create'>New Event</Link></h3>
           <div>
             <h3>Your Events</h3>
-            <ul>
-              
-              {events.map((event, i) => (
-                <li>
-                  <Card className="mb-2" >
-                  <Card.Body className="card-style">
-                    <Card.Title>{event.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Link</Card.Subtitle>
-                    <Card.Text>
-                      {event.date}
-                      <br/>
-                      {event.time}
-                      <br/>
-                      {event.description}
-                    </Card.Text>
-                    <DeleteEvent id={event._id}/>
-                  </Card.Body>
-                  </Card>
-                </li>
-            ))}
-          </ul>
+              <ul>
+                {events.map((event, i) => (
+                  <li>
+                    <Card className="mb-2" >
+                    <Card.Body className="card-style">
+                      <Card.Title>{event.title}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">Card Link</Card.Subtitle>
+                      <Card.Text>
+                        {event.date}
+                        <br/>
+                        {event.time}
+                        <br/>
+                        {event.description}
+                      </Card.Text>
+                      <DeleteEvent id={event._id}/>
+                    </Card.Body>
+                    </Card>
+                  </li>
+              ))}
+            </ul>
+          
         </div>
       </div>
     : <h4>Loading...</h4>
