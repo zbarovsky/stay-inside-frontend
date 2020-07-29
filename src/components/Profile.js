8ac5693896d7c3b9f3d545cd411e3a3aae09e130
Nick Hvattum <nhvattum@gmail.com>
2020-07-29 15:16:28 -0700

    event link works

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap'
 import DeleteEvent from '../events/DeleteEvent'
 import axios from 'axios';
@@ -21,6 +21,7 @@ const Profile = (props) => {
       })
       console.log('call the server for bounties!')
     }, [])
+  
 
   console.log("🦷🦷🦷🦷🦷🦷🦷🦷🦷")
   console.log(props.user)
@@ -33,18 +34,21 @@ const Profile = (props) => {
           <p><strong>ID:</strong> {props.user.id}</p>
           <h3>Create a <Link to='/events/create'>New Event</Link></h3>
           <div>
-            <h3>Your Events</h3>
+            <h3>My Events</h3>
               <ul>
                 {events.map((event, i) => (
                   <li>
                     <Card className="mb-2" >
                     <Card.Body className="card-style">
                       <Card.Title>{event.title}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">Card Link</Card.Subtitle>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
                       <Card.Text>
                         {event.date}
                         <br/>
                         {event.time}
+                        <br/>
+                        <a target="_blank" href={`https://${event.eventLink}`}>{event.eventLink}</a>
+                        
                         <br/>
                         {event.description}
                       </Card.Text>
