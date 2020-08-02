34e5c238bfe5bb68b00a04f4948aace5d1d7e20a
nguyendm77 <nguyendm77@gmail.com>
2020-08-01 18:56:08 -0700

    updated createEvent

--- src/comments/ShowComment.js
+++ src/comments/ShowComment.js
@@ -14,7 +14,7 @@ export default function ShowComment(props) {
     axios.post(`${process.env.REACT_APP_API}/comments/eventcomments`, {data: {comments: comments, event:props.id}})
     .then(response => {
       setComments(response.data)
-      console.log(response.data)
+      // console.log(response.data)
     })
     .catch(err => {
       console.log('🔥🔥🔥🔥')
