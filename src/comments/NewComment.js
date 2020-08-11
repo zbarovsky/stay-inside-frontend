5f15b3f58956bb151823a32323abe08cd7bb73df
nguyendm77 <nguyendm77@gmail.com>
2020-08-11 15:54:49 -0700

    update comment

--- src/comments/NewComment.js
+++ src/comments/NewComment.js
@@ -40,28 +40,28 @@ console.log(props.id)
     return (
         <Redirect to={'/'} /> 
     )
-  } else {
-    return (
-      <Form onSubmit={commentSubmit}>
-        <InputGroup className="mb-3 mt-3">
+  } 
+  
+  return (
+    <Form onSubmit={commentSubmit}>
+      <InputGroup className="mb-3 mt-3">
+      <FormControl
+        aria-describedby="basic-addon2"
+        hidden type="text" name='eventId'
+        value={props.id}
+      />
         <FormControl
-          aria-describedby="basic-addon2"
-          hidden type="text" name='eventId'
-          value={props.id}
-        />
-          <FormControl
-          className='rounded-sm'
-          placeholder="Comment"
-          aria-label="Comment"
-          aria-describedby="basic-addon2"
-          type="text" name='content'
-          onChange={handleInputChange}
-        />
-        <InputGroup.Append>
-          <Button variant="outline-info" type='submit'>Submit</Button>
-        </InputGroup.Append>
-        </InputGroup>
-      </Form>
-    )
-  }
+        className='rounded-sm'
+        placeholder="Comment"
+        aria-label="Comment"
+        aria-describedby="basic-addon2"
+        type="text" name='content'
+        onChange={handleInputChange}
+      />
+      <InputGroup.Append>
+        <Button variant="outline-info" type='submit'>Submit</Button>
+      </InputGroup.Append>
+      </InputGroup>
+    </Form>
+  )
 }
