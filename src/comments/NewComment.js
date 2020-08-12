9eb0f3113ed7c307e317048a56aeb8f31aa50024
nguyendm77 <nguyendm77@gmail.com>
2020-08-11 17:01:06 -0700

    update comment

--- src/comments/NewComment.js
+++ src/comments/NewComment.js
@@ -23,7 +23,8 @@ console.log(props.id)
       axios.post(`${process.env.REACT_APP_API}/comments`, commentInputs)
       .then(response => {
           console.log(response)
-          setCommentCreated(true)
+          setCommentCreated(true,[])
+
       })
       .catch(err => {
         console.log('🔥🔥🔥🔥')
